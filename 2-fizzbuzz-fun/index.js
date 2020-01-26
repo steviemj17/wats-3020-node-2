e350e3fa5c079671ce09ad21fac771f42f1e77e0
steviemj17 <55863655+steviemj17@users.noreply.github.com>
2020-01-25 17:30:45 -0800

    fizzbuzz fun completed

--- 2-fizzbuzz-fun/index.js
+++ 2-fizzbuzz-fun/index.js
@@ -1,26 +1,32 @@
 /*
-Description:
+Description: an adaptation of the FizzBuzz algorithm but with a function
 Input:
 Output:
 Usage: 
 */
 
-// get input using getargs
 const getargs = require("../modules/getargs")
 let input = getargs.getIntegerArg()
 
 
-function fizzbuzz(n){
-// TODO provide for tests for divisible for 3,5, and 15 to return desired output
+function fizzbuzz(n) {
+  if (n % 15 == 0) {
+    return 'fizzbuzz';
+  } else if (n % 3 == 0) {
+    return `fizz`;
+  } else if (n % 5 == 0) {
+    return `buzz`;
+  } else {
+    ``;
+  }
 }
 
-// TODO check if input is not an integer
-if ( ) {
-  // TODO provide usage statement
+if (isNaN(input) || Number.isInteger(input)) {
+  console.log("usage: node 1-fizzbuzz <integer>");
 }
 else {
   // TODO provide info to for loop to iterate from 1 to value of input
-  for ( ) {
+  for (let i = 1; i <= input; i++) {
     console.log(`${i} ${fizzbuzz(i)}`);
   }
 }
